5af671330ec84c8f7a8cd8cdc2f2a2b4ac030f06
Kimberly Cabbagestalk <kimcabb@gmail.com>
2016-03-21 17:40:48 -0400

    updated colors and new crop page

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -1,5 +1,41 @@
 class CropsController < ApplicationController
+
   def index
     @crops = Crop.all
   end
+
+  def new
+    @crop = Crop.new
+  end
+
+  def create
+    @crop = Crop.create
+    redirect_to crops_path(@crop)
+  end
+
+  def show
+    @crop = Crop.find(params[:id])
+  end
+
+  def edit
+    @crop = Crop.find(params[:id])
+  end
+
+  def update
+    @crop = Crop.find(params[:id])
+    @crop.update(crop_params)
+    redirect_to crops_path(@crop)
+  end
+
+  def destroy
+    @crop = Crop.find(params[:id])
+    @crop.destroy
+    redirect_to crops_path
+  end
+
+  private
+  def crop_params
+    params.require(:crop).permit(:name, :season_id)
+  end
+
 end
